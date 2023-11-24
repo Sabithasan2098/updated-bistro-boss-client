2131733f347523ee9e0039cce7ef8c3244574247
Sabithasan2098 <sabithasan2098@gmail.com>
2023-11-24 11:09:41 +0600

    styles

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -4,8 +4,8 @@ const AdminHome = () => {
   const { user } = useAuth();
   return (
     <div>
-      <h1>
-        <span>Hi,welcome</span>
+      <h1 className="text-3xl">
+        <span className="pr-2">Hi,welcome</span>
         {user?.displayName ? user.displayName : "back"}
       </h1>
     </div>
